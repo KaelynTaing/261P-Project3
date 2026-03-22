adf53e96367541cc733454b9fb2bffcca583cd74
give-kk-five <110738266+KaelynTaing@users.noreply.github.com>
2026-03-21 17:05:57 -0700

    Update

--- project3-files-updated/requirements.py
+++ project3-files-updated/requirements.py
@@ -28,16 +28,15 @@ from skip_list import SkipList
 
 # KeyType and ValType are used the same way as in Zip Trees.
 # get_random_level(): Use this function to determine what level each key should be at. Assume levels start at 0 (i.e. the bottom-most list is at level 0)
-#											e.g. for some key x, if get_random_level(x) = 5, then x should be in the lists on levels 0, 1, 2, 3, 4 and 5 in the skip list.
+# 											e.g. for some key x, if get_random_level(x) = 5, then x should be in the lists on levels 0, 1, 2, 3, 4 and 5 in the skip list.
 # insert(): inserts item with parameter key and value into the skip list.
 # remove(): removes item with parameter key from the skip list.
-#						you can assume that the item exists in the skip list.
+# 						you can assume that the item exists in the skip list.
 # find(): returns the value of item with parameter key.
 #         you can assume that the item exists in the skip list.
 # get_list_size_at_level(): returns number of skip list nodes at a given level. Assume levels start at 0 (i.e. the bottom-most list is at level 0).
-#														if the parameter level exceeds the current maximum level in the skip list, return 0.
+# 														if the parameter level exceeds the current maximum level in the skip list, return 0.
 # from_zip_tree(): construct a skip list from the given zip tree as described in the paper (https://arxiv.org/pdf/1806.06726.pdf, "the level-k sublist comprises nodes.
-#				   of the zip tree of rank k or less"). E.g. a node with rank 3 in the zip tree would be in the lists on levels 0, 1, 2 and 3 in the skip list.
-#				   The constructed skip list nodes should have the same key/value information as they do in their corresponding nodes in the zip tree.
-#				   You can assume that this function will only be called immediately after creating a skip list instance (as in the project3_tests.py file)
-
+# 				   of the zip tree of rank k or less"). E.g. a node with rank 3 in the zip tree would be in the lists on levels 0, 1, 2 and 3 in the skip list.
+# 				   The constructed skip list nodes should have the same key/value information as they do in their corresponding nodes in the zip tree.
+# 				   You can assume that this function will only be called immediately after creating a skip list instance (as in the project3_tests.py file)
--- project3-files-updated/skip_list.py
+++ project3-files-updated/skip_list.py
@@ -5,37 +5,102 @@ from typing import TypeVar
 import random
 from zip_tree import ZipTree
 
-KeyType = TypeVar('KeyType')
-ValType = TypeVar('ValType')
+
+KeyType = TypeVar("KeyType")
+ValType = TypeVar("ValType")
+
+
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+
 
 class SkipList:
-	def __init__(self):
-		pass
+    def __init__(self):
+        self.heads = []
+        self.max_lvl = -1
 
-	def get_random_level(self, key: KeyType) -> int:
-	  	# Do not change this function. Use this function to determine what level each key should be at. Assume levels start at 0 (i.e. the bottom-most list is at level 0)
-		# e.g. for some key x, if get_random_level(x) = 5, then x should be in the lists on levels 0, 1, 2, 3, 4 and 5 in the skip list.
-		random.seed(str(key))
-		level = 0
-		while random.random() < 0.5 and level < 20:
-			level += 1
-		return level
+    def get_random_level(self, key: KeyType) -> int:
+        # Do not change this function. Use this function to determine what level each key should be at. Assume levels start at 0 (i.e. the bottom-most list is at level 0)
+        # e.g. for some key x, if get_random_level(x) = 5, then x should be in the lists on levels 0, 1, 2, 3, 4 and 5 in the skip list.
+        random.seed(str(key))
+        level = 0
+        while random.random() < 0.5 and level < 20:
+            level += 1
+        return level
 
-	def insert(self, key: KeyType, val: ValType):
-		pass
+    def insert(self, key: KeyType, val: ValType):
+        lvl = self.get_random_level(key)
 
-	def remove(self, key: KeyType):
-		pass
+        # make new heads list if new level exceeds current max
+        while lvl > self.max_lvl:
+            self.heads.append(Node(None, None))
+            self.max_lvl += 1
+        # insert at every level depending on lvl
+        for l in range(lvl + 1):
+            newnode = Node(key, val)
+            cur = self.heads[l]
+            # insert newnode where it should be in this list
+            while cur.next is not None and cur.next.key < key:
+                cur = cur.next
+            newnode.next = cur.next
+            cur.next = newnode
 
-	def find(self, key: KeyType) -> ValType:
-		pass
+    def remove(self, key: KeyType):
+        # for every possible level
+        for l in range(self.max_lvl + 1):
+            cur = self.heads[l]
+            while cur.next is not None and cur.next.key < key:
+                cur = cur.next
+            if cur.next is not None and cur.next.key == key:
+                cur.next = cur.next.next
 
-	def get_list_size_at_level(self, level: int):
-		pass
-		
-	def from_zip_tree(self, zip_tree: ZipTree) -> None:
-		pass
-			
+    def find(self, key: KeyType) -> ValType:
+        # searching top down
+        for l in range(self.max_lvl, -1, -1):
+            cur = self.heads[l]
+            while cur.next is not None and cur.next.key <= key:
+                cur = cur.next
+                if cur.key == key:
+                    return cur.val
+        return None
+
+    def get_list_size_at_level(self, level: int):
+        if level > self.max_lvl:
+            return 0
+        count = 0
+        cur = self.heads[level].next
+        while cur is not None:
+            count += 1
+            cur = cur.next
+        return count
+
+    def from_zip_tree(self, zip_tree: ZipTree) -> None:
+        # traverse zip tree and insert nodes from level to rank
+        def traverse(node):
+            if node is None:
+                return
+            # set level as the rank
+            level = node.rank
+            while level > self.max_lvl:
+                self.heads.append(Node(None, None))
+                self.max_lvl += 1
+
+            for lvl in range(level + 1):
+                newnode = Node(node.key, node.val)
+                cur = self.heads[lvl]
+                while cur.next is not None and cur.next.key < node.key:
+                    cur = cur.next
+                newnode.next = cur.next
+                cur.next = newnode
+
+            traverse(node.left)
+            traverse(node.right)
+
+        traverse(zip_tree.root)
+
 
 # feel free to define new classes/methods in addition to the above
 # fill in the definitions of each required member function (above),
